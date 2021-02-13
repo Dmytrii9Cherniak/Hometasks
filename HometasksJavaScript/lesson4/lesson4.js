1437bc1bc1f2e9dc5ff57efaab83152b9ad2c56f
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2021-02-13 18:35:59 +0200

    task 18 is not finished

--- HometasksJavaScript/lesson4/lesson4.js
+++ HometasksJavaScript/lesson4/lesson4.js
@@ -340,19 +340,73 @@
 
 // 15 - Создать массив пользователей. У каждого пользователя обязательно должено быть поле skills которое является массивом. Проитерировать массив пользователей и в каждом пользователе проитерировать его массив skills. Скопировать все skills всех пользователей в отедльный массив
 
-let AllUsers = [];
-let usersTask15 = [
-    {name: 'Viktor' , age: 33, status: true, skills: ['C++' , 'Java,' , 'React'],},
-    {name: 'Vasil', age: 27, status: true, skills: ['java' , 'html/css' , 'Node.Js' ],},
-    {name: 'Oles', age: 21, status: true, skills: ['python', 'html/css,' , 'React'],},
+// let AllUsers = [];
+// let usersTask15 = [
+//     {name: 'Viktor' , age: 33, status: true, skills: ['C++' , 'Java,' , 'React'],},
+//     {name: 'Vasil', age: 27, status: true, skills: ['java' , 'html/css' , 'Node.Js' ],},
+//     {name: 'Oles', age: 21, status: true, skills: ['python', 'html/css,' , 'React'],},
+// ];
+
+// for (let user1 of usersTask15){
+//     console.log(user1);
+//     for (let skill of user1.skills){
+//         console.log(skill);
+//         AllUsers.push(skill);
+//     }
+// }
+
+// console.log(AllUsers);
+
+
+
+// 16 - За допомогою 2х циклів циклів проітеррувати  даний масив і масив кожного об'єкта.
+
+//  let users = [{name: 'vasya', age: 31, status: false, skills: ['java', 'js']}, {name: 'petya', age: 30, status: true, skills: ['java', 'js', 'html']}, {name: 'kolya', age: 29, status: true, skills: ['mysql', ',mongo']}, {name: 'olya', age: 28, status: false, skills: ['java', 'js']}, {name: 'max', age: 30, status: true, skills: ['mysql', ',mongo']}];
+
+//  for (let user of users){
+//      console.log(user);
+//      for (let experience of user.skills){
+//         console.log(experience);
+
+//     }
+//  }
+
+
+
+// 17 - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
+
+let users = [
+
+{name: 'vasya', age: 31, status: false, address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}},
+
+{ name: 'petya', age: 30, status: true, address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}}, 
+
+{ name: 'kolya', age: 29, status: true, address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}}, 
+
+{ name: 'olya', age: 28, status: false, address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56} }, 
+
+{ name: 'max', age: 30, status: true, address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39} }, 
+
+{ name: 'anya', age: 31, status: false, address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5} }, 
+
+{ name: 'oleg', age: 28, status: false, address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90} }, 
+
+{ name: 'andrey', age: 29, status: true, address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33} }, 
+
+{ name: 'masha', age: 30, status: true, address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1} }, 
+
+{ name: 'olya', age: 31, status: false, address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4} }, 
+
+{ name: 'max', age: 31, status: true, address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45} }
+
 ];
 
-for (let user1 of usersTask15){
-    console.log(user1);
-    for (let skill of user1.skills){
-        console.log(skill);
-        AllUsers.push(skill);
-    }
-}
+// let ArrayTask17 = [];
+// for(let Array of users){
+//     console.log(Array);
+//     ArrayTask17.push(Array);
+// }
 
-console.log(AllUsers);
+// console.log(ArrayTask17);
+
+// 18 - - За допомоги циклу проітерувати масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Всі данні в одному блоці

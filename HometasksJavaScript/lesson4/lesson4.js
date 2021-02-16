f57c471e6e7bd115d259ad4f6203d29e5f4d444d
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2021-02-16 14:40:14 +0200

    edited mistake in last (21) task

--- HometasksJavaScript/lesson4/lesson4.js
+++ HometasksJavaScript/lesson4/lesson4.js
@@ -479,12 +479,13 @@
 // Записати цей об'єкт в новий масив
 
 // let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false}, {id: 2, name: 'petya', age: 30, status: true}, {id: 3, name: 'kolya', age: 29, status: true}, {id: 4, name: 'olya', age: 28, status: false},];
-//             let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'}, {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, {user_id: 2, country: 'Poland', city: 'Krakow'}, {user_id: 4, country: 'USA', city: 'Miami'},];
 
-// for ( let Array1 of usersWithId ){
-//     for (let Array2 of citiesWithId){
-//         if (Array1.id = Array2.user_id){
-//             Array1.address = Array2;
+// let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'}, {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, {user_id: 2, country: 'Poland', city: 'Krakow'}, {user_id: 4, country: 'USA', city: 'Miami'},];
+
+// for ( let array1 of usersWithId ){
+//     for (let array2 of citiesWithId){
+//         if (array1.id === array2.user_id){
+//             array1.address = array2;
 //         }
 //     }
 // }

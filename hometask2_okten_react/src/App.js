ed7497ceb49731b4aa6c80beed4b8299af21a8eb
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2021-03-30 12:55:44 +0300

    hometask is finished

--- hometask2_okten_react/src/App.js
+++ hometask2_okten_react/src/App.js
@@ -10,74 +10,89 @@ function App() {
     //Завдання 1 та 2
     // Cтворюю масив яким буду користуватися
 
-    let [array, changedArray] = useState([
+    // let [array, changedArray] = useState([
+    //     {
+    //         id: 1,
+    //         title: 'title 1',
+    //         price: 1
+    //     },
+    //     {
+    //         id: 2,
+    //         title: 'title 2',
+    //         price: 2
+    //     },
+    //     {
+    //         id: 3,
+    //         title: 'title 3',
+    //         price: 3
+    //     }
+    // ])
+    //
+    // // Функція відновлення всіх елементів масиву
+    // let initialValue = useRef(array);
+    // function onRestore () {
+    //     changedArray(initialValue.current)
+    // }
+    //
+    // // Функція видалення першого елементу масиву
+    // function removeFirstElement () {
+    //     let newArray = [...array];
+    //     newArray.shift();
+    //     changedArray(newArray);
+    // }
+    //
+    // // Функція видалення останього елементу масиву
+    // function removeLastElement() {
+    //     let newArray = [...array];
+    //     newArray.pop();
+    //     changedArray(newArray)
+    // }
+    // Завдання 3
+
+    let [deletedTasks, setDeletedTasks] = useState([]);
+    let [tasks, setTasks] = useState([
         {
             id: 1,
-            title: 'title 1',
-            price: 1
+            title: 'Task2 title 1'
         },
         {
             id: 2,
-            title: 'title 2',
-            price: 2
+            title: 'Task2 title 2'
         },
         {
             id: 3,
-            title: 'title 3',
-            price: 3
-        }
+            title: 'Task2 title 3'
+        },
     ])
 
-    // Функція відновлення всіх елементів масиву
-    let initialValue = useRef(array);
-    function onRestore () {
-        changedArray(initialValue.current)
-    }
+   let deleteElements = (id) => {
+       const task = tasks.find((e) => e.id === id);
+       setDeletedTasks((p) => [...p, task]);
+       setTasks((p) => p.filter((e) => e.id !== id));
+   }
 
-    // Функція видалення першого елементу масиву
-    function removeFirstElement () {
-        let newArray = [...array];
-        newArray.shift();
-        changedArray(newArray);
-    }
-
-    // Функція видалення останього елементу масиву
-    function removeLastElement() {
-        let newArray = [...array];
-        newArray.pop();
-        changedArray(newArray)
-    }
-    // Завдання 3
-
-    function restoreElements() {
-
-    }
-
-    function delete1element(){
-        let one = document.getElementById('1')
-        one.remove()
-    }
-
-    function delete2element(){
-        let two = document.getElementById('2')
-        two.remove()
-    }
+   let onRevert = () => {
+       setTasks((p) => [...p, ...deletedTasks]);
+       setDeletedTasks([]);
+   }
 
   return (
       <div>
-            <ul>{array.map(el =><li>{el.title} {el.price}</li>)}</ul>
-          <button onClick={removeFirstElement}>Delete first element</button>
-          <button onClick={removeLastElement}>Delete last element</button>
-          <button onClick={onRestore}>Відновити</button>
+          {/*<div>*/}
+          {/*  <ul>{array.map(el =><li>{el.title} {el.price}{el.title}*/}
+          {/*  </li>)}</ul>*/}
+          {/*<button onClick={removeFirstElement}>Delete first element</button>*/}
+          {/*<button onClick={removeLastElement}>Delete last element</button>*/}
+          {/*<button onClick={onRestore}>Відновлення елементів</button>*/}
+          {/*</div>*/}
 
-          <div>
+        <div>
+        <ul>{tasks.map(el =><li>{el.title}
+<button onClick={() => deleteElements(el.id)}>Delete elements</button>
+        </li>)}</ul>
+            <button onClick={onRevert}>Restore</button>
+        </div>
 
-              <button onClick={restoreElements}>Відноивити елементи</button>
-              <ul>
-                  <li id={'1'}>Один</li> <button onClick={delete1element}>Видалити</button>
-                  <li id={'2'}>Два</li><button onClick={delete2element}>Видалити</button>
-              </ul>
-          </div>
       </div>
   );
 }
